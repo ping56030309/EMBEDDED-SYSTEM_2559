20cda64cfd1c999a3bdc298bd27fc0cfe12b4694
ping56030309 <pingping.gh@gmail.com>
2017-05-07 23:06:17 +0700

    Add files via upload

---
+++ Lab_1_LED_SOS/Lab_1_LED_SOS.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+  dot(); dot(); dot();
+  dash(); dash(); dash();
+  dot(); dot(); dot();
+  delay(1400);
+}
+
+
+void dot() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+}
+
+void dash() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+}
+
