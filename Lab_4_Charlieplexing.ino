f422ce86f6a056afdbef53aeb0c54d5623365f46
ping56030309 <pingping.gh@gmail.com>
2017-05-07 23:03:01 +0700

    Add files via upload

---
+++ Lab_4_Charlieplexing.ino
@@ -0,0 +1,70 @@
+const int LED_1 = 10;     //LED row 1
+const int LED_2 = 9;     //LED row 2
+const int LED_3 = 8;     //LED row 3
+
+void setup()
+{
+}
+
+void loop()
+{
+  //turn on LED L1
+  pinMode(LED_1, OUTPUT);     //row 1
+  digitalWrite(LED_1, LOW);
+  pinMode(LED_2, OUTPUT);     //row 2
+  digitalWrite(LED_2, HIGH);
+  pinMode(LED_3, INPUT);      //row 3
+  digitalWrite(LED_3, LOW);
+
+  delay(1000);
+
+  //turn on LED L2
+  pinMode(LED_1, OUTPUT);     //row 1
+  digitalWrite(LED_1, HIGH);
+  pinMode(LED_2, OUTPUT);     //row 2
+  digitalWrite(LED_2, LOW);
+  pinMode(LED_3, INPUT);      //row 3
+  digitalWrite(LED_3, LOW);
+
+  delay(1000);
+
+  //turn on LED L3
+  pinMode(LED_1, INPUT);      //row 1
+  digitalWrite(LED_1, LOW);
+  pinMode(LED_2, OUTPUT);     //row 2
+  digitalWrite(LED_2, LOW);
+  pinMode(LED_3, OUTPUT);     //row 3
+  digitalWrite(LED_3, HIGH);
+
+  delay(1000);
+
+  //turn on LED L4
+  pinMode(LED_1, INPUT);     //row 1
+  digitalWrite(LED_1, LOW);
+  pinMode(LED_2, OUTPUT);    //row 2
+  digitalWrite(LED_2, HIGH);
+  pinMode(LED_3, OUTPUT);    //row 3
+  digitalWrite(LED_3, LOW);
+
+  delay(1000);
+
+  //turn on LED L5
+  pinMode(LED_1, OUTPUT);    //row 1
+  digitalWrite(LED_1, LOW);
+  pinMode(LED_2, INPUT);     //row 2
+  digitalWrite(LED_2, LOW);
+  pinMode(LED_3, OUTPUT);    //row3
+  digitalWrite(LED_3, HIGH);
+
+  delay(1000);
+
+  //turn on LED L6
+  pinMode(LED_1, OUTPUT);
+  digitalWrite(LED_1, HIGH);
+  pinMode(LED_2, INPUT);
+  digitalWrite(LED_2, LOW);
+  pinMode(LED_3, OUTPUT);
+  digitalWrite(LED_3, LOW);
+
+  delay(1000);
+}
